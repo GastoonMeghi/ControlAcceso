f2b74e8e1227cd954496207fa960028732a245c2
Ezequiel <ezeledesma@est.frba.utn.edu.ar>
2017-10-31 09:50:11 -0300

    Descripciones de las funciones de UART1 para el señor don meghi

--- ControlAcceso/src/UART_1/pr_UART1.c
+++ ControlAcceso/src/UART_1/pr_UART1.c
@@ -59,6 +59,17 @@ uint8_t PopRx( uint8_t *dato )
 	return 0;	//dato sacado del buffer
 }
 
+/*
+ * void update_RFID()
+ *
+ * Descripcion:
+ * 		(Esta funcion se debe llamarla en el systick)
+ * 		Cuando la uart1 ya cargo mas de 20 datos en el bufferRx, comprueba si los datos del buffer
+ * 		son los datos de una tarjeta o basura a traves de un byte de start '\002' y de stop '\003'.
+ * 		Si los datos de la tarjeta son correctas, llena otro buffer que lo usa getRFID() para
+ * 		obtener el codigo de la tarjeta (que lo activa y desactiva a traves de un flag).
+ */
+
 void update_RFID() {
 	// Inicio:	valor int = 2, valor ascii = '\002'
 	// Fin:		valor int = 3, valor ascii = '\003'
@@ -81,6 +92,29 @@ void update_RFID() {
 		}
 	}
 }
+
+/*
+ * int get_RFID(uint8_t *dato)
+ *
+ * Argumento:
+ * 		dato es un array de 13 posiciones, en el que se carga el codigo de la tarjeta
+ * 		13 posiciones: 12 datos de la tarjeta + 1 '\0'
+ *
+ * Salida:
+ * 		si no hay tarjeta, ya se leyo o hubo error, devuelve 0
+ * 		si leyo correctamente una tarjeta, devuelve 1
+ *
+ * Ejemplo de uso:
+ *
+ * 		uint8_t aux[13];
+ * 		if(get_RFID(aux)) {
+ *			// Si ingreso al if es porque se ingreso una nueva tarjeta (aux se cargo con el codigo)
+ * 		}
+ * 		else {
+ *		// Si ingreso al else es porque no hay tarjeta, ya se leyo la tarjeta o el buffer tiene basura
+ * 		}
+ *
+ */
 
 int get_RFID(uint8_t *dato) {
 	if (tarjeta == LEIDA) {
